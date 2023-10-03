9586a2c32bceb92bc62d9189549e8de5dbc6a33b
lucgasb <lucgasb@gmail.com>
2023-10-03 15:47:42 -0300

    spawn Test behave added(for FireSpawn and IceSpawn). Ref #16

--- features/spawns.feature
+++ features/spawns.feature
@@ -1,10 +1,16 @@
 Feature: Spawns of cells
 
-  Scenario: First spawn
+  Scenario: First ice spawn
     Given that the setup phase has been completed
-    And the game is waiting
-    When the user choose the position (3, 4) for the spawn and the spawn will create in the position (3, 4)
-    Then the gamemode changes from GameMode.SPAWN_PLACEMENT to GameMode.SIMULATION
+    And the game is waiting for put the ice spawn
+    When the user choose the position (3, 4) for the ice spawn and the ice spawn will create in the position (3, 4)
+    Then the gamemode changes from GameMode.SPAWN_PLACEMENT to GameMode.SIMULATION after the user put the ice spawn
+
+  Scenario: First fire spawn
+    Given that the setup phase has been completed
+    And the game is waiting for put the fire spawn
+    When the user choose the position (3, 4) for the fire spawn and the fire spawn will create in the position (3, 4)
+    Then the gamemode changes from GameMode.SPAWN_PLACEMENT to GameMode.SIMULATION after the user put the fire spawn
 
 #   Scenario: Second spawn
 #     Given that we are playing the game
--- features/steps/spawns_steps.py
+++ features/steps/spawns_steps.py
@@ -1,21 +1,38 @@
 from logic.board import Board
 from logic.spawn import Spawn, IceSpawn, FireSpawn
 from logic.game_state import GameMode, GameState
-###Scenario: First spawn
+###Scenario: First ice spawn
 @given(u'that the setup phase has been completed')
 def step_impl(context):
   pass
 
-@given(u'the game is waiting')
+@given(u'the game is waiting for put the ice spawn')
 def step_impl(context):
   context.state.new_game(rows = 50, columns = 50)
   assert context.state.mode == GameMode.SPAWN_PLACEMENT
 
-@when(u'the user choose the position ({row:d}, {column:d}) for the spawn and the spawn will create in the position ({row:d}, {column:d})')
+@when(u'the user choose the position ({row:d}, {column:d}) for the ice spawn and the ice spawn will create in the position ({row:d}, {column:d})')
 def step_impl(context, row, column):
   spawn = IceSpawn()
   context.state.add_spawn(rows = row, columns = column, spawn = spawn)
 
-@then(u'the gamemode changes from GameMode.SPAWN_PLACEMENT to GameMode.SIMULATION')
+@then(u'the gamemode changes from GameMode.SPAWN_PLACEMENT to GameMode.SIMULATION after the user put the ice spawn')
+def step_impl(context):
+  assert context.state.mode == GameMode.SIMULATION
+
+
+###Scenario: First fire spawn
+
+@given(u'the game is waiting for put the fire spawn')
+def step_impl(context):
+  context.state.new_game(rows = 50, columns = 50)
+  assert context.state.mode == GameMode.SPAWN_PLACEMENT
+
+@when(u'the user choose the position ({row:d}, {column:d}) for the fire spawn and the fire spawn will create in the position ({row:d}, {column:d})')
+def step_impl(context, row, column):
+  spawn = FireSpawn()
+  context.state.add_spawn(rows = row, columns = column, spawn = spawn)
+
+@then(u'the gamemode changes from GameMode.SPAWN_PLACEMENT to GameMode.SIMULATION after the user put the fire spawn')
 def step_impl(context):
   assert context.state.mode == GameMode.SIMULATION
