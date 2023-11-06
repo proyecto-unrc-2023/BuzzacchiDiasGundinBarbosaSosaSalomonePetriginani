93afdfcd7a2a399270f8eda314d6f5bcef09e46a
Fernando Sosa <sosafernando936@gmail.com>
2023-11-06 18:47:59 -0300

    fix behave of fusion. Ref #14

--- features/steps/fusions_steps.py
+++ features/steps/fusions_steps.py
@@ -14,17 +14,18 @@ def step_impl(context, row, column):
 @then(u'a level {level_c1:d} IceCell is created at position ({row:d}, {column:d}) with {life:d} health points and the level {level_c2:d} Ice cells at ({row:d}, {column:d}) disappears from the battlefield')
 def step_impl(context, row, column, life, level_c1, level_c2):
     print(str(context.GameController.get_board().get_box(row, column)))
+    print(str(context.GameController.get_cells(row, column)[0].get_level()))
     assert len(context.GameController.get_cells(row, column)) == 1
     assert isinstance(context.GameController.get_cells(row, column)[0], IceCell)
     assert context.GameController.get_cells(row, column)[0].get_life() == life
-    assert context.GameController.get_cells(row, column)[0].get_level() == level_c1
+    assert context.GameController.get_cells(row, column)[0].get_level() == Level(level_c1)
 
 @then(u'a level {level_c1:d} FireCell is created at position ({row:d}, {column:d}) with {life:d} health points and the level {level_c2:d} Fire cells at ({row:d}, {column:d}) disappears from the battlefield')
 def step_impl(context, row, column, life, level_c1, level_c2):
     assert len(context.GameController.get_cells(row, column)) == 1
     assert isinstance(context.GameController.get_cells(row, column)[0], FireCell)
     assert context.GameController.get_cells(row, column)[0].get_life() == life
-    assert context.GameController.get_cells(row, column)[0].get_level() == level_c1
+    assert context.GameController.get_cells(row, column)[0].get_level() == Level(level_c1)
 
 @then(u'the Ice cells cannot merge, and both coexist two cells level {level_c1:d} with {life_c1:d} health points and other cell level {level_c2:d} with {life_c2:d} health points at position ({row:d}, {column:d})')
 def step_impl(context, row, column, level_c1, level_c2, life_c1, life_c2):
@@ -32,9 +33,9 @@ def step_impl(context, row, column, level_c1, level_c2, life_c1, life_c2):
     assert isinstance(context.GameController.get_cells(row,column)[0], IceCell)
     assert isinstance(context.GameController.get_cells(row,column)[1], IceCell)
     assert context.GameController.get_cells(row,column)[0].get_life() == life_c1
-    assert context.GameController.get_cells(row,column)[0].get_level() == level_c1
+    assert context.GameController.get_cells(row,column)[0].get_level() == Level(level_c1)
     assert context.GameController.get_cells(row,column)[1].get_life() == life_c2
-    assert context.GameController.get_cells(row,column)[1].get_level() == level_c2
+    assert context.GameController.get_cells(row,column)[1].get_level() == Level(level_c2)
     
 @then(u'the Fire cells cannot merge, and both coexist two cells level {level_c1:d} with {life_c1:d} health points and other cell level {level_c2:d} with {life_c2:d} health points at position ({row:d}, {column:d})')
 def step_impl(context, row, column, level_c1, level_c2, life_c1, life_c2):
@@ -42,9 +43,9 @@ def step_impl(context, row, column, level_c1, level_c2, life_c1, life_c2):
     assert isinstance(context.GameController.get_cells(row,column)[0], FireCell)
     assert isinstance(context.GameController.get_cells(row,column)[1], FireCell)
     assert context.GameController.get_cells(row,column)[1].get_life() == life_c1
-    assert context.GameController.get_cells(row,column)[1].get_level() == level_c1
+    assert context.GameController.get_cells(row,column)[1].get_level() == Level(level_c1)
     assert context.GameController.get_cells(row,column)[0].get_life() == life_c2
-    assert context.GameController.get_cells(row,column)[0].get_level() == level_c2
+    assert context.GameController.get_cells(row,column)[0].get_level() == Level(level_c2)
 
 @then(u'a level {level_c1:d} IceCell is created at position ({row:d}, {column:d}) with {life:d} health points and only one level {level_c2:d} IceCell at ({row:d}, {column:d}) with {life_c2:d} health points disappears from the battlefield')
 def step_impl(context, row, column, life, life_c2, level_c1, level_c2):
@@ -53,13 +54,13 @@ def step_impl(context, row, column, life, life_c2, level_c1, level_c2):
     assert isinstance(context.GameController.get_cells(row,column)[0], IceCell)
     assert isinstance(context.GameController.get_cells(row,column)[1], IceCell)
     assert context.GameController.get_cells(row,column)[0].get_life() == life
-    assert context.GameController.get_cells(row,column)[0].get_level() == level_c1
+    assert context.GameController.get_cells(row,column)[0].get_level() == Level(level_c1)
     assert context.GameController.get_cells(row,column)[1].get_life() == life_c2
-    assert context.GameController.get_cells(row,column)[1].get_level() == level_c2
+    assert context.GameController.get_cells(row,column)[1].get_level() == Level(level_c2)
 
 @then(u'a level {level:d} Fire cells is created at position ({row:d}, {column:d}) with {life:d} health points and all level 1 Fire cells at ({row:d}, {column:d}) disappears from the battlefield')
 def step_impl(context, row, column, life, level):
     assert len(context.GameController.get_cells(row, column)) == 1
     assert isinstance(context.GameController.get_cells(row,column)[0], FireCell)
     assert context.GameController.get_cells(row,column)[0].get_life() == life
-    assert context.GameController.get_cells(row,column)[0].get_level() == level
+    assert context.GameController.get_cells(row,column)[0].get_level() == Level(level)
