4452a31a26727dd42c988ecf221a13524fedab34
Fernando Sosa <sosafernando936@gmail.com>
2023-11-20 18:45:43 -0300

    upgrade style of login Ref. #34

--- client/src/App.js
+++ client/src/App.js
@@ -93,21 +93,26 @@ function App() {
   };
 
 return (
-    <div className="container">
-      <h1>Welcome to the Game</h1>
-      <form onSubmit={(event) => event.preventDefault()}>
-        <label htmlFor="team">Select Your Team:</label>
-        <select id="team" name="team" value={team} onChange={handleTeamChange} required>
-          <option value="IceTeam">Ice Team</option>
-          <option value="FireTeam">Fire Team</option>
-        </select>
-        <br />
-        <div id="my-signin2"></div>
-        <br />
-        <button type="button" id="google_sign_out" onClick={handleGoogleSignOut}>
-          Sign Out
-        </button>
-      </form>
+
+  <div className={`container ${team === 'FireTeam' ? 'fire' : 'ice'}`}>
+      <header>
+        <h1>War Of Element</h1>
+      </header>
+      <main>
+        <form className="login-form" onSubmit={(event) => event.preventDefault()}>
+          <label htmlFor="team">Select Your Team:</label>
+          <select id="team" name="team" value={team} onChange={handleTeamChange} required>
+            <option value="IceTeam">Ice Team</option>
+            <option value="FireTeam">Fire Team</option>
+          </select>
+          <br />
+          <div id="my-signin2" className="google-signin"></div>
+          <br />
+          <button type="button" id="google_sign_out" className="sign-out-btn" onClick={handleGoogleSignOut}>
+            Sign Out
+          </button>
+        </form>
+      </main>
     </div>
   );
 }
