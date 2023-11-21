17cd45b6b830bad6958b20e974d9a5781dde9b22
Fernando Sosa <sosafernando936@gmail.com>
2023-11-21 18:38:51 -0300

    avance frontend Ref. #34

--- client/src/Simulation.js
+++ client/src/Simulation.js
@@ -5,16 +5,14 @@ function Simulation() {
   const location = useLocation();
   const [gameState, setGameState] = useState(location.state && location.state.gameState);
   const [winnerTeam, setWinnerTeam] = useState(null);
-  const [lastSpawns, setLastSpawns] = useState(null);
 
   useEffect(() => {
+    let intervalId;
+
     const fetchAndUpdateGameState = async () => {
       try {
-        let updatedSpawns = {};
-
         if (gameState?.mode === 'FINISHED') {
-          // console.log(gameState?.mode);
-          clearInterval(intervalId); 
+          clearInterval(intervalId);
           const winnerResponse = await fetch(`/simulation/get_winner_team`);
           if (winnerResponse.status === 200) {
             const winnerTeam = await winnerResponse.text();
@@ -30,60 +28,85 @@ function Simulation() {
               id: gameState?.id,
             }),
           });
-  
+          
           if (response.status === 200) {
             const responseData = await response.json();
             setGameState(responseData.updated_game_state);
-            updatedSpawns = {
-              iceSpawn: responseData.updated_game_state.ice_spawn,
-              fireSpawn: responseData.updated_game_state.fire_spawn,
-            };
           }
         }
-        console.log(updatedSpawns)
-        setLastSpawns(updatedSpawns);
-
       } catch (error) {
         console.error('Error updating game state:', error);
       }
     };
-  
-    // Interval to call fetchAndUpdateGameState each 2 seconds
-    const intervalId = setInterval(fetchAndUpdateGameState, 1000);
+
+    // Intervalo para llamar a fetchAndUpdateGameState cada 2 segundos
+    intervalId = setInterval(fetchAndUpdateGameState, 1000);
+
+    // Limpiar el intervalo cuando el componente se desmonta o gameState cambia
     return () => clearInterval(intervalId);
   }, [gameState?.id, gameState?.mode]);
 
   return (
-    <div>
-      <h2>Simulation</h2>
-      {winnerTeam ? (
-        <div>
-          <h3>Winner Team:</h3>
-          <p>{winnerTeam}</p>
-        </div>
-      ) : (
-        gameState && (
-          <div>
-            <h3>Game State:</h3>
-            <pre>{JSON.stringify(gameState, null, 2)}</pre>
-            {gameState.board && (
-              <div>
-                <h3>Formatted Board:</h3>
-                <pre>{JSON.stringify(JSON.parse(gameState.board), null, 2)}</pre>
-              </div>
-            )}
-
-            {lastSpawns && (
-              <div>
-                <h3>Last Updated Spawns:</h3>
-                <pre>{JSON.stringify(lastSpawns, null, 2)}</pre>
-              </div>
-            )}
-          </div>
-        )
-      )}
+    <div className="simulation-container">
+      {/* Renderizar el componente Board y pasarle el JSON del tablero */}
+      {gameState && <Board boardData={JSON.parse(gameState.board)} />}
     </div>
   );
 }
+
+const Board = ({ boardData }) => {
+  const getCellClass = (cell) => {
+    // Lógica para determinar la clase de la celda según su contenido
+    if (cell.spawn) {
+      return 'spawn-cell';
+    } else if (cell.fire_cells.length > 0) {
+      return 'fire-cell';
+    } else if (cell.ice_cells.length > 0) {
+      return 'ice-cell';
+    } else {
+      return 'empty-cell';
+    }
+  };
+
+  return (
+    <div className="board-container">
+      {boardData.board.map((row, rowIndex) => (
+        <div key={rowIndex} className="board-row">
+          {row.map((cell, columnIndex) => (
+            <div key={columnIndex} className={`board-cell ${getCellClass(cell)}`}>
+              {/* Puedes personalizar el contenido de cada celda según tus necesidades */}
+              <span>Row: {rowIndex}, Col: {columnIndex}</span>
+              {cell.spawn && (
+                <div>
+                  Spawn: Life {cell.spawn.life}, Type {cell.spawn.type}
+                </div>
+              )}
+              {cell.fire_cells.length > 0 && (
+                <div>
+                  Fire Cells: {JSON.stringify(cell.fire_cells)}
+                </div>
+              )}
+              {cell.ice_cells.length > 0 && (
+                <div>
+                  Ice Cells: {JSON.stringify(cell.ice_cells)}
+                </div>
+              )}
+              {cell.fire_healing_area && (
+                <div>
+                  Fire Healing Area: {JSON.stringify(cell.fire_healing_area)}
+                </div>
+              )}
+              {cell.ice_healing_area && (
+                <div>
+                  Ice Healing Area: {JSON.stringify(cell.ice_healing_area)}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Simulation;
